20d0fca37b48ac4ae4a2d15197e7a17d81cb13b7
Umer Aslam Raza <umeraslamraza1@gmail.com>
2025-06-13 19:12:32 +0500

    xzczx

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'Umer';
+
 const nextConfig = {
   reactStrictMode: false,
   images: {
     unoptimized: true,
   },
   output: 'export',
+  basePath: isProd ? `/${repo}` : '',
 };
 
 export default nextConfig;
