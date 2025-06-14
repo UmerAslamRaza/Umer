98f89b7143957c68343cf5eafda37b7758568422
Umer Aslam Raza <umeraslamraza1@gmail.com>
2025-06-14 21:43:42 +0500

    watermarks

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
 import Layout from '../components/Layout';
-import "../styles/3db657d9fe0478a5.css"
-import "../styles/53a5e31d7ed4f176.css"
-import "../styles/ef46db3751d8e999.css"
-import "../styles/bef403e4b51110e5.css"
-import "../styles/f7237eebd3af2a3f.css"
+import "../styles/styles-1.css"
+import "../styles/styles-2.css"
+import "../styles/styles-4.css"
+import "../styles/styles-3.css"
+import "../styles/styles-5.css"
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
